65b29e821f55fddb5ab7e5ef658ab95326c8bc64
deansayre <deansayre@gmail.com>
2022-05-26 15:10:16 -0400

    fix one_underscore

--- R/one_underscore.R
+++ R/one_underscore.R
@@ -7,11 +7,11 @@
 #'
 #' @examples
 one_underscore <- function(x){
-  if (is.character(x) & !is.na(x)){
+  if(!is.na(x)){
+  if (is.character(x)){
     if(str_count(x, "_")==1){
       a <- str_replace(x, "_", ".")
       if(is_all_numeric(a)){as.numeric(a)}
       return(a)
-    }
-    else return(x)}
+    }}}
   else return(x)}

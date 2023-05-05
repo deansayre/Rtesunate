8f9d4d20b5b21f2badbafa79165cc9b16b4d2022
deansayre <deansayre@gmail.com>
2023-05-05 13:33:26 -0400

    Bowie! Lenny!

--- R/get_jams.R
+++ R/get_jams.R
@@ -16,7 +16,12 @@ get_jams <- function(){
             "https://www.youtube.com/watch?v=6B702b-8_gA",
             "https://www.youtube.com/watch?v=BBXFVFHdmbs",
             "https://www.youtube.com/watch?v=eYwuCpOHKc4",
-            "https://www.youtube.com/watch?v=v6zN5CFL4fs")
+            "https://www.youtube.com/watch?v=v6zN5CFL4fs",
+            "https://www.youtube.com/watch?v=-9prpAv6kvo",
+            "https://www.youtube.com/watch?v=2EntxPIULUI",
+            "https://www.youtube.com/watch?v=uM1fSjwB9cw",
+            "https://www.youtube.com/watch?v=PMavhk16FJU",
+            "https://www.youtube.com/watch?v=lXgkuM2NhYI")
 
   x1 <- sample(vect, 1)
   browseURL(x1)

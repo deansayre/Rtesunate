5f97de7fc34ab057270324e107b031d67d23bcf7
deansayre <deansayre@gmail.com>
2023-05-16 11:31:15 -0400

    DHS data edit (2)

--- R/pmi_dhs_data.R
+++ R/pmi_dhs_data.R
@@ -1,12 +1,12 @@
 #' DHS/MIS malaria data for PMI countries
 #'
-#' Data from DHS/MIS performed in PMI partner countries
+#' Data from DHS/MIS performed in PMI partner countries. Updated 16 May, 2023
 #'
 #' @docType data
 #'
 #' @usage data(pmi_dhs_data)
 #'
-#' @format A data frame with 8735 rows and 28 columns:
+#' @format A data frame with 4462 rows and 13 columns:
 #' \describe{
 #'   \item{CountryName}{Country name}
 #'   \item{SurveyYearLabel}{Year(s) survey was conducted}
@@ -22,13 +22,13 @@
 #'   \item{DHS_CountryCode}{Official two letter DHS country code}
 #'   \item{IndicatorId}{Official indicator ID}
 #'
+#' @examples
+#' data <- pmi_dhs_data
 #'
 #' @keywords datasets
 #'
-#' @references USAID DHS Program
-#' @references rdhs package
-#'
 #' @source \href{https://dhsprogram.com/}{DHS Program}
+#' @source \href{https://cran.r-project.org/web/packages/rdhs/index.html}{rdhs package}
 #'
 
 "pmi_dhs_data"

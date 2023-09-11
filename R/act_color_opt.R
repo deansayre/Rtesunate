e475286f56b15e3a456ee3f6bb482dec7a03e1da
deansayre <deansayre@gmail.com>
2023-09-11 10:50:56 -0400

    function to retrive palette names

---
+++ R/act_color_opt.R
@@ -0,0 +1,16 @@
+#' @title act_color_opt
+#'
+#' @return names of available pre-fab colors
+#'
+#'
+#' @examples
+#' act_show_colors("aladdin_sane")
+#'
+#'
+#'
+#' @export
+
+act_color_opt <- function(){
+  names(mal_pal)
+}
+

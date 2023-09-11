358d781b60ac731eb5ebdd9067f088ca4d5d6ee3
deansayre <deansayre@gmail.com>
2023-09-11 09:35:03 -0400

    added ... to get to linelist::clean_data with additional arguments

--- R/act_clean.R
+++ R/act_clean.R
@@ -5,6 +5,8 @@
 #' @param db Database to clean
 #' @param indiv_num Vector of variables in format of c("x", "y", etc) to retain
 #' as numeric if all whole numbers (which are otherwise converted to factors)
+#' @param ... Additional arameters to pass to linelist's clean_data, including
+#' dictionary for spell changes
 #'
 #' @return A hopefully cleaner database
 #' @export
@@ -12,9 +14,9 @@
 #' @examples
 #' act_clean(db)
 
-act_clean <- function(db, indiv_num = NULL){
+act_clean <- function(db, indiv_num = NULL, ...){
   a <- db %>%
-    linelist::clean_data(guess_dates = F) %>%
+    linelist::clean_data(guess_dates = F, ...) %>%
     dplyr::mutate(dplyr::across(.cols = tidyselect::everything(), ~act_one_under(.x))) %>%
     dplyr::mutate(dplyr::across(tidyselect::where(is.character), ~dplyr::na_if(.x,""))) %>%
     dplyr::mutate(dplyr::across(tidyselect::where(is.character), ~dplyr::na_if(.x,"na"))) %>%
